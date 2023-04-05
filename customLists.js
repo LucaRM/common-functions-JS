d611db4196249298884aed8fab095e6b2a072762
LucaRM <lmalato@hotmail.com>
2023-04-05 15:36:24 -0300

    Encryption function. [In development]

--- customLists.js
+++ customLists.js
@@ -2,6 +2,8 @@ const DevEnv = ["dev", "localhost"];
 
 const linkDelimiter = ["/", ".", ":"];
 
+const criptographKey = "Polar Bears";
+
 module.exports = {
     DevEnv,
     SeparateLink: linkDelimiter,
